7514026ef643b25e59e066070400d73bbd9fc1a5
Ramya Korupolu <104009248+Ramya-korupolu@users.noreply.github.com>
2024-12-18 18:57:14 +0530

    Update powertest.java

--- src/test/powertest.java
+++ src/test/powertest.java
@@ -4,24 +4,24 @@ package powerpackage;
 import org.junit.Test;
 import static org.junit.Assert.*;  
 
-public class Powertest {
+public class PowerTest {
     @Test
     public void one_raised_to_one_is_one() {
-        assertEquals(1, powerfinder.OF(1,1));
+        assertEquals(1, powerFinder.OF(1,1));
     }
 
     @Test
     public void twoRaised_to_one_is_2() {
-        assertEquals(2, powerfinder.OF(2,1));
+        assertEquals(2, powerFinder.OF(2,1));
     }
 
     @Test
     public void two_power_2_is_4() {
-        assertEquals(4, powerfinder.OF(2, 2));
+        assertEquals(4, powerFinder.OF(2, 2));
     }
 
     @Test
     public void three_power_2_Is_Nine() {
-        assertEquals(9, powerfinder.OF(3, 2));
+        assertEquals(9, powerFinder.OF(3, 2));
     }
 }
