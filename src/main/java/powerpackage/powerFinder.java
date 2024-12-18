53da2af0683b2371da41f4497226b0576233a633
Ramya Korupolu <104009248+Ramya-korupolu@users.noreply.github.com>
2024-12-18 19:00:12 +0530

    Update powerFinder.java

--- src/main/java/powerpackage/powerFinder.java
+++ src/main/java/powerpackage/powerFinder.java
@@ -1,7 +1,7 @@
 package powerpackage;
 
 public class PowerFinder {
-    public static int OF(int xyz, int pqr) {
+    public static int calculatePower(int xyz, int pqr) {
         int p = 1;
         for (int i = 0; i < pqr; i++) {
             p *= xyz;
