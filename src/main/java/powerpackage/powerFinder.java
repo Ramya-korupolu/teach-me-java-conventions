d3df1679639f771927fd9435cf4718e90994f097
Ramya Korupolu <104009248+Ramya-korupolu@users.noreply.github.com>
2024-12-18 18:50:41 +0530

    Update and rename powerfinder.java to powerFinder.java

--- src/main/java/powerpackage/powerFinder.java
+++ src/main/java/powerpackage/powerFinder.java
@@ -1,9 +1,9 @@
 package powerpackage;
 
-public class powerfinder {
+public class powerFinder {
     public static int OF(int xyz, int pqr) {
         int p = 1;
-        for (int i = 0; i<pqr; i++) {
+        for (int i = 0; i < pqr; i++) {
             p *= xyz;
         }
         return p;
